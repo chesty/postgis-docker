5ea3dd2d16e1001217651fea42f7266a29cb46b6
Michael Chesterton <michael.chesterton@gmail.com>
2020-06-23 05:10:24 +1000

    update for Dockerfile being moved into subdir

--- bump.sh
+++ bump.sh
@@ -2,28 +2,29 @@
 
 set -x
 
-bump () {
-	BRANCH=$1
-	if [ -z "$BRANCH" ]; then
-		echo "$0 <branch>"
-		exit 1
-	fi
+bump() {
+  BRANCH="$1"
+  if [ -z "$BRANCH" ]; then
+    echo "usage: $0 <branch>"
+    exit 1
+  fi
 
-	if git status | grep -q 'modified:'; then
-    	echo "modified files detected, commit or stash and rerun"
-    	exit 2
-	fi 
+  if git status | grep -q 'modified:'; then
+    echo "modified files detected, commit or stash and rerun"
+    exit 2
+  fi
 
-	git checkout $BRANCH
-	git pull
+  git checkout "$BRANCH"
+  git pull "$BRANCH"
 
-	DATE=`date +%y.%m.%d.1`
+  DATE=$(date +%Y%m%d.1)
 
-	sed -i "s/ENV BUMP .*/ENV BUMP $DATE/" Dockerfile 
-	git stage Dockerfile
-	git commit -m "bump $DATE"
-	git push origin $BRANCH
+  for DF in `find -name 'Dockerfile'`; do
+  	sed -i "s/ENV BUMP .*/ENV BUMP $DATE/" "$DF"
+    git stage "$DF"
+  done
+  git commit -m "bump $DATE"
+  git push origin "$BRANCH"
 }
 
 bump master
-
